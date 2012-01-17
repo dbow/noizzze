4e24fe4fd2b1bcadbe44ca789c9fe3f95cf59138
bowman224 <dbowman123@gmail.com>
2012-01-17 13:42:14 -0800

    Added other arrow to UI w/ Canvas

--- js/muvant.js
+++ js/muvant.js
@@ -20,19 +20,31 @@ MUVANT.Canvas = (function () {
 
         // removeArrow is 35x60px.  addArrow is 35x50px.
         var removeCanvas = document.getElementById('removeArrow'),
-            //addCanvas = document.getElementById('addArrow'),
-            removeCtx = removeCanvas.getContext('2d');
+            addCanvas = document.getElementById('addArrow'),
+            removeCtx = removeCanvas.getContext('2d'),
+            addCtx = addCanvas.getContext('2d');
 
         removeCtx.fillStyle = "black";        
         removeCtx.beginPath();
         removeCtx.moveTo(35, 5);
         removeCtx.lineWidth = 8;
-        removeCtx.quadraticCurveTo(15, 0, 15, 50);
-        removeCtx.moveTo(10, 55);
+        removeCtx.quadraticCurveTo(15, 0, 15, 47);
+        removeCtx.moveTo(11, 55);
         removeCtx.lineTo(25, 40);
         removeCtx.moveTo(15, 55);
         removeCtx.lineTo(5, 40);
         removeCtx.stroke();
+        
+        addCtx.fillStyle = "black";        
+        addCtx.beginPath();
+        addCtx.moveTo(30, 50);
+        addCtx.lineWidth = 8;
+        addCtx.quadraticCurveTo(20, 40, 15, 10);
+        addCtx.moveTo(12, 5);
+        addCtx.lineTo(25, 20);
+        addCtx.moveTo(17, 5);
+        addCtx.lineTo(5, 20);
+        addCtx.stroke();
         
       } else {
         log('no canvas');
